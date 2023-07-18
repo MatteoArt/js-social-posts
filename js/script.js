ec616e635e43b56855a1dae1cfaaf8b39374b6a3
MatteoArt <mattrim8@gmail.com>
2023-07-18 16:55:55 +0200

    completo milestone 1

---
+++ js/script.js
@@ -0,0 +1,58 @@
+/* creo array di oggetti che rappresentano i singoli post */
+
+const posts = [
+    {
+        id: 1,
+        author: "John Doe",
+        profile: "https://unsplash.it/300/300?image=12",
+        date: "2020-05-12",
+        text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        image: "https://unsplash.it/600/400?image=200",
+        likes: 10
+    },
+    {
+        id: 2,
+        author: "Martin Ford",
+        profile: "https://unsplash.it/300/300?image=45",
+        date: "2022-06-23",
+        text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        image: "https://unsplash.it/600/400?image=250",
+        likes: 34
+    },
+    {
+        id: 3,
+        author: "George Stone",
+        profile: "https://unsplash.it/300/300?image=67",
+        date: "2023-01-10",
+        text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        image: "https://unsplash.it/600/400?image=567",
+        likes: 21
+    },
+    {
+        id: 4,
+        author: "Richard Miller",
+        profile: "https://unsplash.it/300/300?image=157",
+        date: "2023-02-09",
+        text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        image: "https://unsplash.it/600/400?image=900",
+        likes: 50
+    },
+    {
+        id: 5,
+        author: "Elena Rossi",
+        profile: "https://unsplash.it/300/300?image=202",
+        date: "2021-11-12",
+        text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        image: "https://unsplash.it/600/400?image=1000",
+        likes: 55
+    },
+    {
+        id: 6,
+        author: "Corey Taylor",
+        profile: "https://unsplash.it/300/300?image=345",
+        date: "2023-05-20",
+        text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        image: "https://unsplash.it/600/400?image=867",
+        likes: 70
+    }
+];
